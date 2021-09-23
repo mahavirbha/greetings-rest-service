6788b3ed9bbd5a9a9e682460102fff2d83e612bc
Mahavirbha <robowithsomeaix@gmail.com>
2021-09-23 23:28:25 +0530

    Initial commit

---
+++ src/main/java/com/example/greetingsrestservice/GreetingsRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.greetingsrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingsRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingsRestServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/greetingsrestservice/GreetingsRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greetingsrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingsRestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
